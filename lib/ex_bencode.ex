8937f115273cd94be91140ce6477d68f75b97d38
Rosa Richter <rosa.richter@protonmail.com>
2017-12-20 13:08:01 -0600

    New Mix project

---
+++ lib/ex_bencode.ex
@@ -0,0 +1,18 @@
+defmodule ExBencode do
+  @moduledoc """
+  Documentation for ExBencode.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExBencode.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_bencode_test.exs
@@ -0,0 +1,8 @@
+defmodule ExBencodeTest do
+  use ExUnit.Case
+  doctest ExBencode
+
+  test "greets the world" do
+    assert ExBencode.hello() == :world
+  end
+end
