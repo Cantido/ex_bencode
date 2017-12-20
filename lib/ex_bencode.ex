fa2b30a5f6397b70cfe3785c8e53c0c77a8897ec
Rosa Richter <rosa.richter@protonmail.com>
2017-12-20 14:07:41 -0600

    Implement integer parsing

--- lib/ex_bencode.ex
+++ lib/ex_bencode.ex
@@ -4,15 +4,49 @@ defmodule ExBencode do
   """
 
   @doc """
-  Hello world.
+  Decode the bencoded binary value.
 
   ## Examples
 
-      iex> ExBencode.hello
-      :world
+      iex> ExBencode.decode(nil)
+      {:ok, nil}
+
+      iex> ExBencode.decode("")
+      {:ok, nil}
+
+      iex> ExBencode.decode("i10e")
+      {:ok, 10}
+
+      iex> ExBencode.decode("i8001e")
+      {:ok, 8001}
+
+      iex> ExBencode.decode("i")
+      {:error, :not_bencoded_form}
 
   """
-  def hello do
-    :world
+  def decode(b)
+
+
+  def decode(nil) do
+    {:ok, nil}
+  end
+
+  def decode("i") do
+    {:error, :not_bencoded_form}
+  end
+
+  def decode(s) do
+    case String.length(s) do
+      0 -> {:ok, nil}
+      x when x <= 2 -> {:error, :not_bencoded_form}
+      _ -> {:ok, extract_int(s)}
+    end
+  end
+
+  defp extract_int(s) do
+    len = String.length(s)
+    substr = String.slice(s, 1, (len - 2))
+    {int, _} = Integer.parse(substr)
+    int
   end
 end
--- test/ex_bencode_test.exs
+++ test/ex_bencode_test.exs
@@ -1,8 +1,4 @@
 defmodule ExBencodeTest do
   use ExUnit.Case
   doctest ExBencode
-
-  test "greets the world" do
-    assert ExBencode.hello() == :world
-  end
 end
