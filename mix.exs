1f0e3a56b06b9cd1f01304a2224095ba5387aea9
Rosa Richter <rosa.richter@protonmail.com>
2017-12-21 10:33:10 -0600

    Prepare for 1.0.0 release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExBencode.Mixfile do
   def project do
     [
       app: :ex_bencode,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       description: "An Elixir library for encoding and decoding BitTorrent's bencoding.",
@@ -24,8 +24,7 @@ defmodule ExBencode.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
   end
 
