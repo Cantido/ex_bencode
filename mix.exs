face02f72ea2cacab8a8de5ca399a5f7eb5c1143
Rosa Richter <rosa.richter@asynchrony.com>
2018-05-24 11:42:38 -0500

    Bump version to 2.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExBencode.Mixfile do
   def project do
     [
       app: :ex_bencode,
-      version: "2.0.0",
+      version: "2.0.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       description: "An Elixir library for encoding and decoding BitTorrent's bencoding.",
