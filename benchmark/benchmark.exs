4a170df358d76e6bbfccc3e83c93a7d90280d3ec
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-10-15 22:08:26 -0600

    Remove "performance test"
    that's a bad way to do it

--- benchmark/benchmark.exs
+++ benchmark/benchmark.exs
@@ -3,16 +3,27 @@ map = list |> Enum.map(&to_string/1) |> Enum.with_index() |> Map.new()
 
 Benchee.run(
   %{
-    "ex_bencode encoding a list" => fn -> ExBencode.encode!(list) end,
-    "bento encoding a list" => fn -> Bento.encode!(list) end
+    "ExBencode encoding a list" => fn -> ExBencode.encode!(list) end,
+    "Bento encoding a list" => fn -> Bento.encode!(list) end
   }
 )
-
-IO.puts ""
 
 Benchee.run(
   %{
-    "ex_bencode encoding a map" => fn -> ExBencode.encode!(map) end,
-    "bento encoding a map" => fn -> Bento.encode!(map) end
+    "ExBencode encoding a map" => fn -> ExBencode.encode!(map) end,
+    "Bento encoding a map"     => fn -> Bento.encode!(map) end
   }
 )
+
+file = File.read!("test/linuxmint-18.3-cinnamon-64bit.iso.torrent")
+{:ok, meta} = ExBencode.decode(file)
+
+Benchee.run(%{
+  "ExBencode decoding a torrent" => fn -> ExBencode.decode(file) end,
+  "Bento decoding a torrent"     => fn -> Bento.decode(file) end
+}, memory_time: 2, parallel: 2)
+
+Benchee.run(%{
+  "ExBencode encoding a torrent" => fn -> ExBencode.encode(meta) end,
+  "Bento encoding a torrent"     => fn -> Bento.encode(meta) end
+}, memory_time: 2, parallel: 2)
