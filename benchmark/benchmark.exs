ab47e2a771a6a63b70e640e4ad69ce651a2dc148
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-10-15 22:51:05 -0600

    Add a decoding benchmark

--- benchmark/benchmark.exs
+++ benchmark/benchmark.exs
@@ -1,5 +1,8 @@
-list = Enum.to_list(1..100_000)
+list = Enum.to_list(1..1_000_000)
 map = list |> Enum.map(&to_string/1) |> Enum.with_index() |> Map.new()
+
+{:ok, blist} = ExBencode.encode(list)
+{:ok, bmap} = ExBencode.encode(map)
 
 Benchee.run(
   %{
@@ -14,6 +17,21 @@ Benchee.run(
     "Bento encoding a map"     => fn -> Bento.encode!(map) end
   }
 )
+
+Benchee.run(
+  %{
+    "ExBencode decoding a list" => fn -> ExBencode.encode!(blist) end,
+    "Bento decoding a list" => fn -> Bento.encode!(blist) end
+  }
+)
+
+Benchee.run(
+  %{
+    "ExBencode decoding a map" => fn -> ExBencode.encode!(bmap) end,
+    "Bento decoding a map"     => fn -> Bento.encode!(bmap) end
+  }
+)
+
 
 file = File.read!("test/linuxmint-18.3-cinnamon-64bit.iso.torrent")
 {:ok, meta} = ExBencode.decode(file)
